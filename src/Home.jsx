b98ac0013a66f40f9abc7ee3fd16f47988a5307d
vishal <vishalbabar7351@gmail.com>
2024-09-17 13:20:07 +0530

    lectureNum3

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
 
     <section id="hero">
         <div class="hero-content">
-            <h1>I Am Home Page!</h1>
+            <h1>I Am Home Page</h1>
         </div>
     </section>
 
